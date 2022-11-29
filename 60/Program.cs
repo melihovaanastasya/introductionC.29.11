71f36eab54a8ae188f1a90d0b88240c085f3d0ec
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-29 22:00:20 +0300

    60

---
+++ 60/Program.cs
@@ -0,0 +1,24 @@
+﻿// 60. Показать двумерный массив размером m×n заполненный целыми случайными числами
+
+int[,] Random2DArray(int m,int n,int min=0,int max=10)
+{
+    int[,] a=new int[m,n];
+    Random random=new Random();
+    for(int i=0;i<m;i++)
+         for(int j=0;j<n;j++)
+                a[i,j]=random.Next(min,max+1);
+    return a;
+}
+
+void Print2DArray(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+        {
+     for(int j=0;j<a.GetLength(1);j++)
+        System.Console.Write($"{a[i,j],4}");
+           System.Console.WriteLine();
+        }
+}
+
+int[,] a=Random2DArray(5,6);
+Print2DArray(a);
